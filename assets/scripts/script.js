9a5adec887401215f0e41f28ec6d7a742d9bb088
grahamhub <grammar@hey.com>
2021-08-19 10:20:48 -0500

    style: use css animation

--- assets/scripts/script.js
+++
@@ -1,9 +0,0 @@
-const words = ["builds", "deploys", "releases"];
-let i = 0;
-
-const _changeText = () => {
-  i = (i + 1) % words.length;
-  document.getElementById("spin").innerHTML = words[i];
-}
-
-setInterval("_changeText()", 800);
